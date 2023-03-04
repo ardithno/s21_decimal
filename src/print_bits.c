030f93a4f574104d8b67646483b99a3f2dcc7626
Nikolay Kiryanov <nikolay@kiryanov.ru>
2023-03-04 15:44:07 +0300

    `s21_sub` and minimal tests + fix conversion error + test for it

--- src/print_bits.c
+++ src/print_bits.c
@@ -6,6 +6,15 @@
 
 #include "s21_decimal.h"
 
+uint32_t git_int_bits_as_uint(int x) {
+  union convert {
+    int int_value;
+    uint32_t uint_value;
+  } convert = {.int_value = x};
+
+  return convert.uint_value;
+}
+
 void s21_mul_int(s21_decimal *input, int multiplier) {
   s21_decimal x = *input;
 
@@ -25,8 +34,9 @@ void s21_mul_int(s21_decimal *input, int multiplier) {
 
 int main() {
   // 792.28162514264337593543950335 + 10 = 802.2816251426433759354395034
-  s21_decimal x = {.bits = {0xffffffff, 0xffffffff, 0xffffffff, 0x1a0000}};
-  s21_decimal y = {.bits = {1, 0, 0, 0}};
+  s21_decimal x = {.bits = {1, 1, 1, 65536}};
+  s21_decimal y = {.bits = {1, 1, 1, 655360}};
+  // s21_decimal expect = {.bits = {0x3b9aca01, 0x3b9aca01, 0x3b9aca01, 0xa0000}};
   // s21_decimal expect = {.bits = {0xe399999a, 0xafad9ae1, 0x19a1df2e, 0x190000}};
   // s21_decimal result = S21_DECIMAL_NULL;
 
@@ -42,4 +52,6 @@ int main() {
   _s21_big_decimal result = S21_DECIMAL_NULL;
   result = _s21_big_decimal_add(&big_x, &big_y);
   print_bits(sizeof(result), &result);
+
+  printf("-2147418112 as uint = %u\n", git_int_bits_as_uint(-2147418112));
 }
