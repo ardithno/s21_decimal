6da0fa67fede9b61c3c9c0c90d7667241c82e6e8
ardithno <antikyanartem@gmail.com>
2023-03-14 14:10:13 +0300

    lint

--- src/tests/tests_s21_from_decimal_to_int.c
+++ src/tests/tests_s21_from_decimal_to_int.c
@@ -4,64 +4,60 @@
 #include "test_cases.h"
 
 START_TEST(int_max) {
-    int value = INT_MAX;
-    int result = 0;
-    s21_decimal src = {{INT_MAX, 0, 0, 0}};
-    int status = s21_from_decimal_to_int(src, &result);
-    ck_assert_int_eq(result, value);
-    ck_assert_int_eq(status, 0);
+  int value = INT_MAX;
+  int result = 0;
+  s21_decimal src = {{INT_MAX, 0, 0, 0}};
+  int status = s21_from_decimal_to_int(src, &result);
+  ck_assert_int_eq(result, value);
+  ck_assert_int_eq(status, 0);
 }
 
 START_TEST(int_min) {
-    int value = INT_MIN;
-    int result = 0;
-    s21_decimal src = {{INT_MIN, 0, 0, 0}};
-    s21_change_sign(&src);
-    int status = s21_from_decimal_to_int(src, &result);
-    ck_assert_int_eq(result, value);
-    ck_assert_int_eq(status, 0);
+  int value = INT_MIN;
+  int result = 0;
+  s21_decimal src = {{INT_MIN, 0, 0, 0}};
+  s21_change_sign(&src);
+  int status = s21_from_decimal_to_int(src, &result);
+  ck_assert_int_eq(result, value);
+  ck_assert_int_eq(status, 0);
 }
 
 START_TEST(overflow) {
-    int result = 0;
-    s21_decimal src = {{INT_MIN, 0, 0, 0}};
-    int status = s21_from_decimal_to_int(src, &result);
-    ck_assert_int_eq(status, 1);
+  int result = 0;
+  s21_decimal src = {{INT_MIN, 0, 0, 0}};
+  int status = s21_from_decimal_to_int(src, &result);
+  ck_assert_int_eq(status, 1);
 }
-
 
 START_TEST(normal_int) {
-    s21_decimal src = {{133141, 0, 0, 0}};
-    int status = 0;
-    int result = 0;
-    status = s21_from_decimal_to_int(src, &result);
-    ck_assert_int_eq(result, 133141);
-    ck_assert_int_eq(status, 0);
+  s21_decimal src = {{133141, 0, 0, 0}};
+  int status = 0;
+  int result = 0;
+  status = s21_from_decimal_to_int(src, &result);
+  ck_assert_int_eq(result, 133141);
+  ck_assert_int_eq(status, 0);
 }
 END_TEST
-
 
 START_TEST(normal_negative_int) {
-    s21_decimal src = {{123451234, 0, 0, 0}};
-    int status = 0;
-    int result = 0;
-    s21_change_sign(&src);
-    status = s21_from_decimal_to_int(src, &result);
-    ck_assert_int_eq(result, -123451234);
-    ck_assert_int_eq(status, 0);
+  s21_decimal src = {{123451234, 0, 0, 0}};
+  int status = 0;
+  int result = 0;
+  s21_change_sign(&src);
+  status = s21_from_decimal_to_int(src, &result);
+  ck_assert_int_eq(result, -123451234);
+  ck_assert_int_eq(status, 0);
 }
 END_TEST
-
 
 START_TEST(overflow_with_scale) {
-    s21_decimal src = {{1714336475, 602887423, 48737584, 2148401152}};
-    int status = 0;
-    int result = 0;
-    status = s21_from_decimal_to_int(src, &result);
-    ck_assert_int_eq(status, 1);
+  s21_decimal src = {{1714336475, 602887423, 48737584, 2148401152}};
+  int status = 0;
+  int result = 0;
+  status = s21_from_decimal_to_int(src, &result);
+  ck_assert_int_eq(status, 1);
 }
 END_TEST
-
 
 START_TEST(int_with_big_pow) {
   s21_decimal src = {{344, 4294967295, 0, 0}};
@@ -70,10 +66,8 @@ START_TEST(int_with_big_pow) {
   s21_set_power(&src, 11);
   s21_from_decimal_to_int(src, &result);
   ck_assert_int_eq(value, result);
-
 }
 END_TEST
-
 
 START_TEST(int_and_decimal_nul) {
   s21_decimal value_1 = {0};
@@ -91,7 +85,6 @@ START_TEST(int_and_decimal_nul) {
   s21_from_decimal_to_int(value_1, &b);
   ck_assert_int_eq(value_1.bits[0], test.bits[0]);
   ck_assert_int_eq(s21_get_sign(value_1), 1);
-
 }
 END_TEST
 
@@ -108,6 +101,6 @@ TCase *tcase_s21_from_decimal_to_int(void) {
   tcase_add_test(tc, overflow_with_scale);
   tcase_add_test(tc, int_with_big_pow);
   tcase_add_test(tc, int_and_decimal_nul);
-  
+
   return tc;
 }
